0af5d6641c6e503738d8150871b27be189d256a4
Jean-Louis Giordano <jean-louis@jawaninja.com>
2015-01-29 10:22:24 +0100

    Fix

--- Casks/virtualbox4310.rb
+++ Casks/virtualbox4310.rb
@@ -2,7 +2,7 @@ cask :v1 => 'virtualbox4310' do
   version '4.3.10-93012'
   sha256 '8bf24a7afbde0cdb560b40abd8ab69584621ca6de59026553f007a0da7b4d443'
   url "http://download.virtualbox.org/virtualbox/#{version.sub(/-.*$/, '')}/VirtualBox-#{version}-OSX.dmg"
-  name 'VirtualBox'
+
   homepage 'http://www.virtualbox.org'
   license :gpl
   tags :vendor => 'Oracle'
