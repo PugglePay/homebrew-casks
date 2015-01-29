2489a2452ce2017cb19cdcf0677fe49836d53559
Jell <jean-louis@jawaninja.com>
2015-01-29 09:28:45 +0100

    [87181024] Update cask definitions

--- Casks/vagrant162.rb
+++ Casks/vagrant162.rb
@@ -1,8 +1,13 @@
-class Vagrant162 < Cask
-  url 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.2.dmg'
-  homepage 'http://www.vagrantup.com'
+cask :v1 => 'vagrant' do
   version '1.6.2'
   sha256 '471443c5912a9e02ebed53a8f8acf05c3faed643de54e986ecbd355755976bc5'
+
+  # bintray.com is the official download host per the vendor homepage
+  url "https://dl.bintray.com/mitchellh/vagrant/vagrant_#{version}.dmg"
+  homepage 'http://www.vagrantup.com'
+  license :mit
+
   install 'Vagrant.pkg'
-  uninstall :script => { :executable => 'uninstall.tool', :input => %w[Yes] }
+  uninstall :script => { :executable => 'uninstall.tool', :input => %w[Yes] },
+            :pkgutil => 'com.vagrant.vagrant'
 end
--- Casks/virtualbox4310.rb
+++ Casks/virtualbox4310.rb
@@ -1,8 +1,16 @@
-class Virtualbox4310 < Cask
-  url 'http://download.virtualbox.org/virtualbox/4.3.10/VirtualBox-4.3.10-93012-OSX.dmg'
-  homepage 'http://www.virtualbox.org'
+cask :v1 => 'virtualbox4310' do
   version '4.3.10-93012'
   sha256 '8bf24a7afbde0cdb560b40abd8ab69584621ca6de59026553f007a0da7b4d443'
-  install 'VirtualBox.pkg'
-  uninstall :script => { :executable => 'VirtualBox_Uninstall.tool', :args => %w[--unattended] }
+  url "http://download.virtualbox.org/virtualbox/#{version.sub(/-.*$/, '')}/VirtualBox-#{version}-OSX.dmg"
+  name 'VirtualBox'
+  homepage 'http://www.virtualbox.org'
+  license :gpl
+  tags :vendor => 'Oracle'
+
+  pkg 'VirtualBox.pkg'
+  binary '/Applications/VirtualBox.app/Contents/MacOS/VBoxManage'
+  binary '/Applications/VirtualBox.app/Contents/MacOS/VBoxHeadless'
+
+  uninstall :script => { :executable => 'VirtualBox_Uninstall.tool', :args => %w[--unattended] },
+            :pkgutil => 'org.virtualbox.pkg.*'
 end
